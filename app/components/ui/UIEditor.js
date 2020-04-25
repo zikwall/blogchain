a29ef8873f1ec8577d9d931ceb611501a3ca0ae4
zikwall <andrey.kapitonov.96@gmail.com>
2020-04-26 00:35:32 +0300

    Added toolbar modules

--- app/components/ui/UIEditor.js
+++ app/components/ui/UIEditor.js
@@ -7,14 +7,17 @@ const QuillNoSSRWrapper = dynamic(
 
 const defModules = {
     toolbar: [
-        [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-        [{ size: [] }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }, { 'font': [] }],
+        [{ 'color': [] }, { 'background': [] }],
+        [{ 'size': ['small', false, 'large', 'huge'] }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],
         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ 'list': 'ordered' }, { 'list': 'bullet' },
-            { 'indent': '-1' }, { 'indent': '+1' }],
+        [{ 'align': [] }],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
         ['link', 'image', 'video', 'formula'],
         ['clean'],
         ['code-block'],
+
     ],
     syntax: true,
 };
@@ -23,7 +26,7 @@ const defFormats = [
     'header', 'font', 'size',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
     'list', 'bullet', 'indent',
-    'link', 'image', 'video', 'code-block', 'formula'
+    'link', 'image', 'video', 'code-block', 'formula', 'script', 'super', 'sub', 'color', 'background', 'align'
 ];
 
 const UIEditor = ({ modules, formats, onChange, initialValue }) => {
