f43e987aeb7cb893675cc56ae4896c3b859e67d3
zikwall <andrey.kapitonov.96@gmail.com>
2020-04-29 11:16:14 +0300

    Pass href to tags for optimizing SEO

--- app/components/ui/UITagbar.js
+++ app/components/ui/UITagbar.js
@@ -1,14 +1,23 @@
+import React from 'react';
 import { Label } from 'semantic-ui-react';
 import Link from 'next/link';
+
+const TagLink = React.forwardRef(({ onClick, href, tag, tagget }, ref) => {
+    return (
+        <a href={href} onClick={onClick} ref={ref}>
+            <Label as='span' horizontal tag={tagget} pointing={tagget}>
+                { tag.name }
+            </Label>
+        </a>
+    )
+});
 
 const UITagBar = ({ tags, tagget }) => {
     return (
         <div className='tagget-content'>
             {tags.map((v, k) => (
-                <Link key={k} href={`/tag/[tag]`} as={`/tag/${v.label}`}>
-                    <Label key={k} as='a' horizontal tag={tagget} pointing={tagget}>
-                        { v.name }
-                    </Label>
+                <Link key={k} href={`/tag/[tag]`} as={`/tag/${v.label}`} passHref>
+                    <TagLink tag={v} tagget={tagget} />
                 </Link>
             ))}
         </div>
