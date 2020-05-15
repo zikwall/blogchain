9be1be9d8029ba3daa5e9d7718584113b3044d69
zikwall <andrey.kapitonov.96@gmail.com>
2020-05-15 15:39:52 +0300

    Review initialize method: bugfix another cookie has (not token, user) & review

--- app/services/auth/Initialize.js
+++ app/services/auth/Initialize.js
@@ -7,9 +7,24 @@ import Session from "./Session";
 // checks if the page is being loaded on the server, and if so, get auth token from the cookie:
 export default (ctx) => {
     if(ctx.isServer) {
+
+        // if has cookies & has token -- try auth
         if(ctx.req.headers.cookie) {
-            const user = JSON.parse(Cookie.getCookie(USER_KEY, ctx.req));
-            const token = Cookie.getCookie(SESSION_TOKEN_KEY, ctx.req);
+            const ucookie = Cookie.getCookie(USER_KEY, ctx.req);
+            const tcookie = Cookie.getCookie(SESSION_TOKEN_KEY, ctx.req);
+
+            if (typeof tcookie === "undefined" || typeof ucookie === "undefined") {
+                return false;
+            }
+
+            let user = null;
+            let token = tcookie;
+
+            try {
+                user = JSON.parse(ucookie);
+            } catch (e) {
+                // do stuff
+            }
 
             if (token && Session.isSessionExpired(token)) {
                 ctx.store.dispatch(deauthenticate());
@@ -17,18 +32,18 @@ export default (ctx) => {
                 ctx.store.dispatch(reauthenticate(token, user));
             }
         }
-    } else {
-        let token = ctx.store.getState().authentication.token;
 
-        if (token && Session.isSessionExpired(token)) {
-            token = null;
-            ctx.store.dispatch(deauthenticate());
-        }
+        return true;
+    }
 
-        if(token && (ctx.pathname === '/login' || ctx.pathname === '/register')) {
-            setTimeout(function() {
-                Router.push('/');
-            }, 0);
-        }
+    let token = ctx.store.getState().authentication.token;
+
+    if (token && Session.isSessionExpired(token)) {
+        token = null;
+        ctx.store.dispatch(deauthenticate());
+    }
+
+    if(token && (ctx.pathname === '/login' || ctx.pathname === '/register')) {
+        Router.push('/');
     }
 };
