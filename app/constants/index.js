725e828b9284290a874d878f94585e9159a8af8c
zikwall <andrey.kapitonov.96@gmail.com>
2020-05-15 16:14:49 +0300

    Rename cookie constants

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,8 +1,8 @@
 export const API_DOMAIN = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
     ? 'http://127.0.0.1:3001'
     : 'http://127.0.0.1:3001';
-export const SESSION_TOKEN_KEY = 'token';
-export const USER_KEY = '__buk__identifier';
+export const SESSION_TOKEN_KEY = '__blogchain_token';
+export const USER_KEY = '__blogchain_identifier';
 export const IMAGES_CDN = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
     ? 'http://127.0.0.1:3001/uploads'
     : 'http://127.0.0.1:3001/uploads';
