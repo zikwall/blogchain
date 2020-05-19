b166a01b7ed22d6c874161d3ac0c9e0d3ab7e05c
zikwall <andrey.kapitonov.96@gmail.com>
2020-05-19 17:28:30 +0300

    Review profile sidebar: split to components

--- app/components/ui/UIProfileSidebar.js
+++ app/components/ui/UIProfileSidebar.js
@@ -1,76 +1,118 @@
-import {Button, Header, Icon, Image, Label} from "semantic-ui-react";
+import { useSelector } from "react-redux";
+import { Button, Header, Icon, Image, Label } from "semantic-ui-react";
+import { getToken, getUser } from "@blogchain/redux/reducers";
 
-const UIProfileSidebar = ({ user }) => {
-    const avatar = !!user.profile.avatar ? user.profile.avatar : "/images/zebra_pl.jpg";
+const UIProfileImage = ({ status, avatar }) => (
+    <div style={{
+        border: '1px solid rgba(0,0,0,.1)',
+    }}>
+        <Image
+            src={avatar}
+            size='medium'
+        />
+
+        {
+            !!status &&
+            <div style={{
+                padding: '10px',
+            }}>
+                {status}
+            </div>
+        }
+
+    </div>
+);
+
+const UIProfileName = ({ name, username }) => (
+    <div style={{ paddingTop: '10px', paddingBottom: '10px' }}>
+        <Header as="h1">
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <span>{name}</span>
+            </div>
+        </Header>
+        <span style={{
+            fontHeight: 300,
+            fontSize: '24px',
+            lineHeight: '14px',
+        }}>{username}</span>
+    </div>
+);
+
+const UIProfileEditButton = ({ id }) => {
+    const isAuthorized = useSelector(state => !!getToken(state));
+    const authorizedUser = useSelector(state => getUser(state));
 
     return (
-        <div style={{width: '300px'}}>
-            <div style={{
-                border: '1px solid rgba(0,0,0,.1)',
-            }}>
-                <Image
-                    src={avatar}
-                    size='medium'
-                />
 
-                {
-                    !!user.profile.status &&
-                    <div style={{
-                        padding: '10px',
-                    }}>
-                        {user.profile.status}
-                    </div>
-                }
-
-            </div>
-
-            <div style={{ paddingTop: '10px', paddingBottom: '10px' }}>
-                <Header as="h1">
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column'
-                    }}>
-                        <span>{user.profile.name}</span>
-                    </div>
-                </Header>
-                <span style={{
-                    fontHeight: 300,
-                    fontSize: '24px',
-                    lineHeight: '14px',
-                }}>{user.username}</span>
-            </div>
-
+        (isAuthorized && authorizedUser.id === id) &&
+        <>
             <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
                 <Button animated='fade' basic fluid>
                     <Button.Content visible>Редактировать профиль</Button.Content>
                     <Button.Content hidden>Приступить</Button.Content>
                 </Button>
             </div>
+        </>
+    )
+};
 
-            {
-                !!user.profile.description &&
-                <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
-                    { user.profile.description }
-                </div>
-            }
+const UIProfileDescription = ({ description }) => (
+    !!description &&
+    <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
+        { description }
+    </div>
+);
 
-            <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
-                {
-                    !!user.profile.email &&
-                    <Label basic>
-                        <Icon name='send'/> {user.profile.email}
-                    </Label>
-                }
-                {
-                    !!user.profile.location &&
-                    <>
-                        <div style={{paddingTop: '5px'}}/>
-                        <Label basic>
-                            <Icon name='map marker alternate'/> Russian, Moscow
-                        </Label>
-                    </>
-                }
-            </div>
+const UIProfileLocationWithContacts = ({ email, location }) => (
+    <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
+        {
+            !!email &&
+            <Label basic>
+                <Icon name='send'/> {email}
+            </Label>
+        }
+        {
+            !!location &&
+            <>
+                <div style={{paddingTop: '5px'}}/>
+                <Label basic>
+                    <Icon name='map marker alternate'/> Russian, Moscow
+                </Label>
+            </>
+        }
+    </div>
+);
+
+const UIProfileSidebar = ({ user }) => {
+    const avatar = !!user.profile.avatar ? user.profile.avatar : "/images/zebra_pl.jpg";
+
+    return (
+        <div style={{width: '300px'}}>
+            <UIProfileImage
+                avatar={avatar}
+                status={user.profile.status}
+            />
+
+            <UIProfileName
+                username={user.username}
+                name={user.profile.name}
+            />
+
+            <UIProfileEditButton
+                id={user.id}
+            />
+
+            <UIProfileDescription
+                description={user.profile.description}
+            />
+
+            <UIProfileLocationWithContacts
+                email={user.profile.email}
+                location={user.profile.location}
+            />
         </div>
     )
 };
