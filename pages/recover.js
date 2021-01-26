888876c880b9b00532946cda8909bcc35c08b14f
zikwall <andrey.kapitonov.96@gmail.com>
2021-01-26 19:32:08 +0300

    Add register and recover password pages

---
+++ pages/recover.js
@@ -0,0 +1,16 @@
+import { WithoutHeaderLayout } from "@blogchain/layouts";
+import Head from "next/head";
+
+const Recover = () => {
+    return (
+        <WithoutHeaderLayout>
+            <Head>
+                <title>Recover password | Blogchain</title>
+            </Head>
+
+            Recover page
+        </WithoutHeaderLayout>
+    )
+};
+
+export default Recover;
