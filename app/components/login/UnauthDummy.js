3b21ca3d0e2dc1ba5e1db28727ad44ecffdfb851
zikwall <andrey.kapitonov.96@gmail.com>
2021-01-31 19:11:27 +0300

    Fix warning: Anonymous arrow functions cause Fast Refresh to not preserve local component state

--- app/components/login/UnauthDummy.js
+++ app/components/login/UnauthDummy.js
@@ -1,5 +1,5 @@
 /* if user is not authorized see bottom sheet */
-export default function ({ visible }) {
+export default function UnauthDummy({ visible }) {
     return (
         <div className='unauth-dummy' style={{
             paddingBottom: visible ? '80px' : '0px'
