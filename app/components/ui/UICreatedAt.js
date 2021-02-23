2e79b33cdaac60da50c5728d48fbd6d7ee7aa6fa
zikwall <andrey.kapitonov.96@gmail.com>
2021-02-23 22:10:59 +0300

    Theming article list ui

--- app/components/ui/UICreatedAt.js
+++ app/components/ui/UICreatedAt.js
@@ -1,11 +1,23 @@
 import moment from "moment";
 import { Time } from "@blogchain/utils";
 import { Label } from "semantic-ui-react";
+import { useThemeContext } from "@blogchain/components";
 
-const UICreatedAt = ({ time }) => (
-    <Label as='a' basic>
-        {moment(Time.toJsUnix(time)).fromNow()}
-    </Label>
-);
+const UICreatedAt = ({ time }) => {
+    const [ theme ] = useThemeContext();
+    const additionalProps = {};
+
+    if (theme.isDark) {
+        additionalProps['color'] = 'black';
+    } else {
+        additionalProps['basic'] = true;
+    }
+
+    return (
+        <Label as='a' {...additionalProps}>
+            {moment(Time.toJsUnix(time)).fromNow()}
+        </Label>
+    )
+};
 
 export default UICreatedAt;
--- app/components/ui/UILabelBar.js
+++ app/components/ui/UILabelBar.js
@@ -1,24 +1,36 @@
-import { Icon, Label } from "semantic-ui-react";
 import Link from "next/link";
+import { Icon, Label } from "semantic-ui-react";
+import { useThemeContext } from "@blogchain/components";
 
-export const UILabelBar = ({ link, ratings, views, bookmarks, comments }) => (
-    <>
-        <Label basic pointing>
-            <Icon name='lightning' /> { ratings }
-        </Label>
-        <Label basic pointing>
-            <Icon name='eye' /> { views }
-        </Label>
-        <Label basic pointing>
-            <Icon name='bookmark' /> { bookmarks }
-        </Label>
-        <Link href={`/post/[id]/#comments`} as={`${link}#comments`}>
-            <Label basic color='blue' pointing as='a'>
-                Comments
-                <Label.Detail>{ comments }</Label.Detail>
+export const UILabelBar = ({ link, ratings, views, bookmarks, comments }) => {
+    const [ theme ] = useThemeContext();
+    const additionalProps = {};
+
+    if (theme.isDark) {
+        additionalProps['color'] = 'black';
+    } else {
+        additionalProps['basic'] = true;
+    }
+
+    return (
+        <>
+            <Label pointing {...additionalProps}>
+                <Icon name='lightning' /> { ratings }
             </Label>
-        </Link>
-    </>
-);
+            <Label pointing {...additionalProps}>
+                <Icon name='eye' /> { views }
+            </Label>
+            <Label pointing {...additionalProps}>
+                <Icon name='bookmark' /> { bookmarks }
+            </Label>
+            <Link href={`/post/[id]/#comments`} as={`${link}#comments`}>
+                <Label {...additionalProps} color='blue' pointing as='a'>
+                    Comments
+                    <Label.Detail>{ comments }</Label.Detail>
+                </Label>
+            </Link>
+        </>
+    )
+};
 
 export default UILabelBar;
