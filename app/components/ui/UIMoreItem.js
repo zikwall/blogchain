47004b1b0b57599335533bcff5f9957e0735d43b
zikwall <andrey.kapitonov.96@gmail.com>
2021-02-28 19:29:06 +0300

    Themed more of author and company info components

--- app/components/ui/UIMoreItem.js
+++ app/components/ui/UIMoreItem.js
@@ -1,23 +1,30 @@
 import { Label, Header, Divider, Icon } from 'semantic-ui-react';
+import { useThemeContext } from "@blogchain/components";
 
-const MoreLabels = ({ views, comments }) => (
-    <>
-        <Label basic>
-            <Icon name='eye' /> { views }
-        </Label>
-        <Label basic as="a">
-            <Icon name='comment' /> { comments }
-        </Label>
-    </>
-);
+const MoreLabels = ({ views, comments }) => {
+    const [ theme ] = useThemeContext();
 
-const UIMoreItem = ({ labels, title, href }) => {
     return (
         <>
-            <Header as="h4">
+            <Label basic>
+                <Icon name='eye' /> { views }
+            </Label>
+            <Label basic as="a">
+                <Icon name='comment' /> { comments }
+            </Label>
+        </>
+    )
+};
+
+const UIMoreItem = ({ labels, title, href }) => {
+    const [ theme ] = useThemeContext();
+
+    return (
+        <>
+            <Header as="h4" inverted={theme.isDark}>
                 <a href={href} style={{
                     textDecoration: 'none',
-                    color: 'rgba(0,0,0,.87)'
+                    color: theme.isDark ? '#ffffff' : 'rgba(0,0,0,.87)'
                 }}>
                     { title }
                 </a>
