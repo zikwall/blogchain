2d12c197da12d52fe8b1455877f43822a21d8646
zikwall <andrey.kapitonov.96@gmail.com>
2021-07-27 14:44:22 +0300

    Improve: main app use functional component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import { Provider } from "react-redux";
-import App from "next/app";
 import ErrorPage from 'next/error';
 import withRedux from "next-redux-wrapper";
 import { MainLayout } from "@blogchain/layouts";
@@ -19,65 +18,50 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-class BlogchainClientApplication extends App {
-    static async getInitialProps({ Component, ctx }) {
-
-        /**
-         * Initial session of user
-         */
-        BootstrappedContext(ctx);
-
-        /**
-         * Initial props in pages
-         * @type {{}}
-         */
-        const props = {
-            ...(Component.getInitialProps
-                ? await Component.getInitialProps(ctx)
-                : {})
-        };
-
-        /**
-         * Set http status code for Next SEO, it's real HTTP code not props
-         */
-        if (props.statusCode && ctx.res) {
-            ctx.res.statusCode = props.statusCode
-        }
-
-        return {
-            pageProps: props
-        };
+function ClientApplication({ Component, pageProps, store }) {
+    /**
+     * Handle HTTP OK status equal is 2** code
+     * Get initial props in pages return statusCode, example:
+     *
+     * ```js
+     *
+     * Post.getInitialProps = async ({ query, res }) => {
+     *      const { id } = query;
+     *      const { status, content, statusCode } = await Content.GetContent(id);
+     *      return { content: content, statusCode: statusCode }
+     * };
+     *
+     * ```
+     */
+    if (pageProps.statusCode && !Http.isOk(pageProps.statusCode)) {
+        return <ErrorPage statusCode={pageProps.statusCode} />
     }
 
-    render() {
-        const { Component, pageProps, store } = this.props;
-
-        /**
-         * Handle HTTP OK status equal is 2** code
-         * Get initial props in pages return statusCode, example:
-         *
-         * ```js
-         *
-         * Post.getInitialProps = async ({ query, res }) => {
-         *      const { id } = query;
-         *      const { status, content, statusCode } = await Content.GetContent(id);
-         *      return { content: content, statusCode: statusCode }
-         * };
-         *
-         * ```
-         */
-        if (pageProps.statusCode && Http.isOk(pageProps.statusCode) === false) {
-            return <ErrorPage statusCode={pageProps.statusCode} />
-        }
-
-        return (
-            <Provider store={ store }>
-                <MainLayout>
-                    <Component {...pageProps} />
-                </MainLayout>
-            </Provider>
-        )
-    }
+    return (
+        <Provider store={ store }>
+            <MainLayout>
+                <Component {...pageProps} />
+            </MainLayout>
+        </Provider>
+    )
 }
 
-export default withRedux(makeStore)(BlogchainClientApplication);
+ClientApplication.getInitialProps = async ({ ctx, Component }) => {
+    // initial session of user
+    BootstrappedContext(ctx);
+
+    const appProps = {
+        statusCode: 0, ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {})
+    };
+
+    // set HTTP status code for Next SEO, it's real HTTP code not props
+    if (appProps.statusCode && ctx.res) {
+        ctx.res.statusCode = appProps.statusCode
+    }
+
+    return {
+        pageProps: appProps
+    };
+}
+
+export default withRedux(makeStore)(ClientApplication);
